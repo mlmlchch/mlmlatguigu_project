83f8bfd68bbb130ebbb72f0443443fbd1f089eeb
ch <1538132792@qq.com>
2021-10-10 14:08:30 +0800

    hotfix

--- src/main/java/com/atguigu/Test.java
+++ src/main/java/com/atguigu/Test.java
@@ -3,5 +3,8 @@ package com.atguigu;
 public class Test {
     public static void main(String[] args) {
         System.out.println("1111");
+        System.out.println("2222");
+        System.out.println("4444");
+        System.out.println("1111");
     }
 }
